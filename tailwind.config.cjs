3bebe5a21391d78ed4acaf5599cabc3a0e15c2cf
JOYBOY-0 <falamino00@gmail.com>
2022-08-26 03:07:25 -0300

    add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
     theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+      'seconday': ['Inter', 'sans-serif'],
+      'primary': ['Montserrat', 'sans-serif'],
+      }
+    }
   },
   plugins: []
 }
