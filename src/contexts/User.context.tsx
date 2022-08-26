7b8e4f9005a38ced836c3790c51df050ce4f467b
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-08-26 04:21:28 -0300

    add authguard to protect routes and check if theres user

--- src/contexts/User.context.tsx
+++ src/contexts/User.context.tsx
@@ -1,20 +1,15 @@
 import { User } from '@/models'
 import React, { createContext, useContext, useState } from 'react'
 
-type UserContext = {
+export type UserContext = {
   user: User | null
   login: (user: User) => void
   logout: () => void
   update: (newUser: Partial<User>) => void
 }
-const userContext = createContext<UserContext>({
-  user: null,
-  login: () => {},
-  logout: () => {},
-  update: () => {}
-})
+const userContext = createContext<UserContext | null>(null)
 
-const useUserContext = () => useContext(userContext)
+const useUserContext = () => useContext(userContext) as UserContext
 
 type Props = {
   children: React.ReactNode
