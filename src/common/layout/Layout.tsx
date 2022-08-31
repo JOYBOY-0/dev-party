d85d9bffdf1e36e357fe9ca3a593e514cca35588
JOYBOY-0 <falamino00@gmail.com>
2022-08-31 12:57:36 -0300

    made layout component styles more customizable

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,17 +1,28 @@
 import React, { FC } from 'react'
-
 type Props = {
     children? : React.ReactNode,
-    className?: string
+    className?: string,
+    color? : string,
+    pattern? : string,
 }
 
-export const Layout : FC<Props> = (props) => {
+export const Layout : FC<Props> = ({
+  className, color = "#0f172a", pattern, ...props
+}) => {
+
+  const bgPattern = "/svg/bg_pattern_slate_800.svg"
   return (
     <div 
-        className={`flex justify-center items-center
-        w-screen min-h-screen
-        m-auto max-h-screen max-w-[100vw]
-        overflow-hidden ${props.className}`}
+      style={{
+        background: `url(${bgPattern}), ${color}`,
+        backgroundPosition: 'top left',
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'clamp(300px, 15vw , 400px)'
+      }}
+      className={`flex justify-center items-center
+      w-screen min-h-screen transition-all
+      m-auto max-h-screen max-w-[100vw]
+      overflow-hidden ${className}`}
     >
     {props.children}
   </div>
