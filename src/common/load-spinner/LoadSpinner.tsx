1be2ae65a10cba9a43b67a84e72ed0733aaef63e
JOYBOY-0 <falamino00@gmail.com>
2022-09-01 01:37:26 -0300

    fixed uppercase import causing deploy error

--- src/common/load-spinner/LoadSpinner.tsx
+++ src/common/load-spinner/LoadSpinner.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react'
 import { HTMLAttributes } from 'react'
-import './LoadSpinner.css'
+import './loadSpinner.css'
 
 
 export const LoadSpinner : FC<HTMLAttributes<HTMLDivElement>> = (props) => {
