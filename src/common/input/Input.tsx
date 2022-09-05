9c612a8088d77efd033e5a9befa3de99efd17890
JOYBOY-0 <falamino00@gmail.com>
2022-09-05 02:38:50 -0300

    add customization for label style

--- src/common/input/Input.tsx
+++ src/common/input/Input.tsx
@@ -1,13 +1,15 @@
 import React, { AllHTMLAttributes, FC } from 'react'
 
 interface Props extends AllHTMLAttributes<HTMLInputElement> {
-    label?: string
+    label?: string,
+    tag?: boolean,
     placeholder?: string,
     name?: string,
 }
 
 export const Input : FC<Props> = ({
     label,
+    tag,
     id,
     name,
     type = 'text',
@@ -16,21 +18,19 @@ export const Input : FC<Props> = ({
     onChange,
     disabled,
     className,
+    children,
     ...props
 }) => {
 
   return (
-    <div className='flex flex-col'>
-        {label && 
-        <label
-        htmlFor={id}
-        className='auth_label'
-        >
-            {label}
-        </label>}
+    <div className='flex flex-col-reverse items-start font-primary group w-full max-w-xs relative'>
+
         <input 
-        className={`w-full p-2 text-slate-100 placeholder:text-slate-400 
-        rounded-md shadow-inner bg-slate-600 max-w-xs ${className}`}
+        className={`w-full p-2 
+        text-slate-100 placeholder:text-slate-400
+        rounded-md shadow-inner bg-slate-600 peer
+        focus:outline-none ring-2 ring-slate-500 focus:ring-devPink-600
+        ${className}`}
         id={id}
         name={name ? name : id}
         type={type}
@@ -40,6 +40,15 @@ export const Input : FC<Props> = ({
         onChange={onChange}
         {...props}
         />
+        {children}
+        {label && 
+        <label
+            htmlFor={id}
+            className={`${tag ? "ml-4 peer-focus:bg-devPink-600 bg-slate-500 rounded-t-lg px-4" : "mb-1.5" } 
+            py-1 text-white uppercase `}
+        >
+            {label}
+        </label>}
   </div>
   )
 }
