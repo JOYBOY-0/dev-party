666ca7cadb0d3aa76f69399f10452f80fe7503eb
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-09-27 19:48:15 -0300

    refactoring CategoryCard with theme

--- src/assets/svg-components/CategoryCard.tsx
+++ src/assets/svg-components/CategoryCard.tsx
@@ -1,7 +1,5 @@
+import { CategoryTheme } from '@/theme/CategoryTheme'
 import { FC } from 'react'
-import { ProductivityIcon } from '../icons'
-import CodeIcon from '../icons/CodeIcon'
-import DesignIcon from '../icons/DesignIcon'
 import IconCard from './IconCard'
 
 type Props = {
@@ -11,42 +9,16 @@ type Props = {
 }
 
 export const CategoryCard: FC<Props> = ({ category, className, onClick }) => {
-  if (category === 0) {
-    return (
-      <IconCard
-        className={className}
-        text='Productivity'
-        fill='#0F172A'
-        background='var(--devyellow-400)'
-        onClick={onClick}
-      >
-        <ProductivityIcon fill='#0F172A' y={'-20px'} />
-      </IconCard>
-    )
-  }
-  if (category === 1)
-    return (
-      <IconCard
-        className={className}
-        text='Design'
-        fill='#FFFF'
-        background='var(--devpink-600)'
-        onClick={onClick}
-      >
-        <DesignIcon fill='#FFF' height={'75%'} y={'10'} />
-      </IconCard>
-    )
-  if (category === 2)
-    return (
-      <IconCard
-        className={className}
-        text='Code'
-        fill='#FFF'
-        background='var(--devblue-600)'
-        onClick={onClick}
-      >
-        <CodeIcon fill='#FFF' y={'-20px'} />
-      </IconCard>
-    )
-  return null
+  const Icon = CategoryTheme[category].icon
+  return (
+    <IconCard
+      className={className}
+      text={CategoryTheme[category].name}
+      fill={CategoryTheme[category].textColor}
+      background={CategoryTheme[category].backgroundColor}
+      onClick={onClick}
+    >
+      <Icon fill={CategoryTheme[category].textColor} y={'-20px'} />
+    </IconCard>
+  )
 }
